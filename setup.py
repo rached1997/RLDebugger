27910b0369b5cc4646b0f0a6a006586c8a2e5d4d
ahmedhajyahmed <hajyahmedahmed@yahoo.fr>
2023-01-29 12:05:47 -0500

    Added initial project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+# TODO: Change this and use setuptools
+from distutils.core import setup
+
+setup(name='RLDebugger',
+      version='0.1',
+      description='',
+      author='Rached and Ahmed',
+      packages=['debugger'],
+     )
