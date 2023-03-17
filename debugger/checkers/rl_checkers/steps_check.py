23d6ed903200f812e8ced18dae80ec8431608a3e
rached1997 <bouchoucharached@gmail.com>
2023-03-16 21:09:40 -0400

    add more doc-strings

--- debugger/checkers/rl_checkers/steps_check.py
+++ debugger/checkers/rl_checkers/steps_check.py
@@ -1,19 +1,19 @@
 import statistics
-
-import numpy as np
-import torch
-
 from debugger.config_data_classes.rl_checkers.steps_config import StepsConfig
 from debugger.debugger_interface import DebuggerInterface
 
 
 class StepCheck(DebuggerInterface):
+    """
+    This class performs checks on the maximum number of steps that can be reached .
+    For more details on the specific checks performed, refer to the `run()` function.
+    """
     def __init__(self):
         """
         Initializes the following parameters:
-        final_step_number_buffer :  list storing the number of steps taken in each episode
-        episode_reward_buffer : lis storing the rewards accumulated in each episode
-        last_step_num : an in meantioning in the previous episode what was the totla number of steps the agent did
+            * final_step_number_buffer :  list storing the number of steps taken in each episode
+            * episode_reward_buffer : list storing the rewards accumulated in each episode
+            * last_step_num : an integetr storing the total number of steps the agent did in the previous episode
         """
         super().__init__(check_type="Step", config=StepsConfig)
         self.final_step_number_buffer = []
@@ -22,17 +22,30 @@ class StepCheck(DebuggerInterface):
 
     def run(self, reward, max_reward, max_total_steps, max_steps_per_episode) -> None:
         """
-        Checks if episodes are being ended prematurely due to the max step limit being reached during the
-        exploitation phase when the agent is not learning (i.e. the reward is far from the max reward).
+        -----------------------------------   I. Introduction of the Step Check   -----------------------------------
+        The number of steps represents the number of times that the agent interacts with its environment. In DRL,
+        every episode is defined by a maximum number of steps, and once that limit is reached, the experiment starts
+        over. It's essential to set an appropriate value for the maximum number of steps per episode to avoid having
+        episodes that end prematurely, which can reduce the learning efficiency of the agent.
+
+        The primary goal of this class is to ensure that episodes are not being ended prematurely due to the maximum
+        step limit being reached, especially during the exploitation phase when the agent is not learning,
+        and the reward is far from the maximum reward.
+
+        ------------------------------------------   II. The performed checks  -----------------------------------------
 
         The steps check performs the following check:
             (1) Checks whether the max steps per episode has a low value.
 
+        ------------------------------------   III. The potential Root Causes  -----------------------------------------
+
         The potential root causes behind the warnings that can be detected are:
             - The max steps per episode has a low value (checks triggered : 1)
 
+        --------------------------------------   IV. The Recommended Fixes  --------------------------------------------
+
         The recommended fixes for the detected issues :
-            - Increase the max stepsper episode value (checks that can be fixed: 1)
+            - Increase the max step number per episode value (checks that can be fixed: 1)
 
         Args:
             reward (float): the cumulative reward collected in one episode
--- debugger/checkers/rl_checkers/value_function_check.py
+++ debugger/checkers/rl_checkers/value_function_check.py
@@ -12,15 +12,24 @@ class ValueFunctionCheck(DebuggerInterface):
         self, targets, steps_rewards, discount_rate, predicted_next_vals, steps_done
     ) -> None:
         """
-        This checks is usefull for the DRL applicactions that uses q-value based learning. The q-value is calculated
-        based on the bellman equation. Which is generally implemented manually by the developper. The goal of this
-        check is to make sure that the bellman equation is being applied correctly.
+        -----------------------------------   I. Introduction of the Value function Check   -----------------------------------
+
+        This check is particularly useful for DRL applications that use Q-value based learning, as the Q-value is
+        used to calculate the Bellman equation. Accurately calculating the Bellman equation is crucial to avoid
+        errors and ensure optimal learning efficiency of the DRL agent.
+        Therefore, this check is responsible for verifying that the Bellman equation is being applied correctly
+
+        ------------------------------------------   II. The performed checks  -----------------------------------------
 
         The value function check performs the following check:
             (1) Checks whether the bellman equation is applied correctly.
 
+        ------------------------------------   III. The potential Root Causes  -----------------------------------------
+
         The potential root causes behind the warnings that can be detected are:
             - A coding error : The bellman is not calculated correctly (checks triggered : 1)
+
+        --------------------------------------   IV. The Recommended Fixes  --------------------------------------------
 
         The recommended fixes for the detected issues :
             - Fix the calculation of the Bellman function (checks that can be fixed: 1)
@@ -31,8 +40,6 @@ class ValueFunctionCheck(DebuggerInterface):
             discount_rate: the gamma value required to apply the bellman equation
             predicted_next_vals: the predicted next q values
             steps_done: the list of flags of each step being done or not
-
-        Returns:
 
         """
         q_targets = steps_rewards + discount_rate * predicted_next_vals * (
